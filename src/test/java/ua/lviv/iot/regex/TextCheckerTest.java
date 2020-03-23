8e7077154535b05760c6365598df07336d9c3914
Apollo1207 <fednyakillya12072001@gmail.com>
2020-03-23 13:34:21 +0200

    fix bugs

--- src/test/java/ua/lviv/iot/regex/TextCheckerTest.java
+++ src/test/java/ua/lviv/iot/regex/TextCheckerTest.java
@@ -1,8 +1,23 @@
 package ua.lviv.iot.regex;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class TextCheckerTest {
+
+//    @Test
+//       public void inputText() {
+//        String stringInput = "something";
+//        InputStream inputStreamForString = new ByteArrayInputStream(stringInput.getBytes());
+//        Scanner scannerForString = new Scanner(inputStreamForString);
+//        String string = scannerForString.nextLine();
+//        String replaceText = string.replaceAll("[^A-Za-zА-Я\\s]", "");
+//        TextChecker.checkPatterns(replaceText);
+//    }
+
 
     public static void main(String... args) {
         Scanner in = new Scanner(System.in);
@@ -12,5 +27,4 @@ public class TextCheckerTest {
         String replaceText = text.replaceAll("[^A-Za-zА-Я\\s]", "");
         TextChecker.checkPatterns(replaceText);
     }
-
 }
