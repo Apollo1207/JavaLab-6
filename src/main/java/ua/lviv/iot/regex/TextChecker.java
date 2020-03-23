68544de98817aae9b5e887f439ebee5cee734649
Apollo1207 <fednyakillya12072001@gmail.com>
2020-03-23 12:34:20 +0200

    final version lab6

--- src/main/java/ua/lviv/iot/regex/TextChecker.java
+++ src/main/java/ua/lviv/iot/regex/TextChecker.java
@@ -6,119 +6,44 @@ import java.util.regex.Pattern;
 
 public class TextChecker {
 
-    public static void checkPatterns(final String str) {
-        ArrayList<String> foundStrings = new ArrayList<>();
-        Pattern pattern = Pattern.compile("\\s[^!.?]");
-        String[] strings = pattern.split(str);
-        System.out.println(str);
-        for (String s : strings) {
-            foundStrings.add(s);
+    public static void checkPatterns(final String textToCheck) {
+        String textToChecks = textToCheck.trim().replaceAll("\\s{2,}", " ");
 
+        LinkedList<String> words = new LinkedList<>();
+        Pattern pattern = Pattern.compile("\\s");
+        String[] formatTextToCheck = pattern.split(textToChecks);
+
+        for (String s : formatTextToCheck) {
+            words.add(s);
         }
-        System.out.println(foundStrings);
 
-        for (String string : foundStrings) {
-            int count = 0;
-            Pattern vowelPattern = Pattern.compile("[aeiou]");
-            Matcher vowelMatcher = vowelPattern.matcher(string);
 
+        Iterator<String> wordsIterator = words.iterator();
+        while (wordsIterator.hasNext()) {
+            String word = wordsIterator.next();
+
+            int countVowels = 0;
+            Pattern vowelPattern = Pattern.compile("[aeiouAEIOU]");
+            Matcher vowelMatcher = vowelPattern.matcher(word);
             while (vowelMatcher.find()) {
-                count++;
+                countVowels++;
             }
-            System.out.println(count);
-            int counts = 0;
-            Pattern vowelPatterns = Pattern.compile("[dbklctr]");
-            Matcher vowelMatchers = vowelPatterns.matcher(string);
-            while (vowelMatchers.find()) {
-                counts++;
-            }
-            System.out.println(counts);
 
-            if (count > counts) {
-                foundStrings.remove(string);
 
+            int countConsonant = 0;
+            Pattern consonantPatterns = Pattern.compile("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]");
+            Matcher consonantMatchers = consonantPatterns.matcher(word);
+            while (consonantMatchers.find()) {
+                countConsonant++;
             }
-            System.out.println(foundStrings);
+
+
+            if (countVowels > countConsonant) {
+                wordsIterator.remove();
+            }
+
         }
-
-
-//        while (m.find()) {
-//                     foundStrings.add(m.group());
-//        }
-
-
-//        String[] words = pattern.split(str);
-
-
-//                    Matcher matcher = pattern.matcher(word);
-//                    while (matcher.find()) {
-//                        foundStrings.add(matcher.group());
-
-
-//        String[] words = pattern.split(str);
-//        System.out.println(words);
-//        List<String> lists = new ArrayList<String>(Arrays.asList(words));
-//        System.out.println(lists);
-//        System.out.println(lists);
-//                Pattern pattern = Pattern.compile("[ ,.!?]");
-//               String[] words = pattern.split(str);
-//        System.out.println(words);
-
-//        List<String> lists = new ArrayList<String>(Arrays.asList(words));
-//
-//
-//
-
-
-//
-//        System.out.println(count);
-
-
-//        String pattern = "(?i)[aeiou]";
-//        System.out.println(textToCheck);
-//        System.out.println(textToCheck.replaceAll(pattern, "a").length());
-
-//        String line = "aa";
-//        String pattern = "[aeiou]";
-//        System.out.println(line.replaceAll(pattern, "").length());
-//    }
-//}
-//        String pattern = "(?i)[aeiou]";
-//
-//        Pattern r = Pattern.compile(pattern);
-//
-//        Matcher m = r.matcher(textToCheck);
-//
-//        while (m.find()) {
-//            System.out.println(m.group(0));
-//        }
-//
-//    }
-
-//    public static void check(List<String> lists) {
-//        for (String list : lists) {
-//            Pattern patterns = Pattern.compile("[aeoui]");
-//            Matcher matchers = patterns.matcher(list);
-//            List<String> is = new ArrayList<>();
-//            while (matchers.find()) {
-//                is.add(matchers.group());
-//            }
-//            System.out.println(is);
-//        }
-//
-//    }
-//
-//
-//    public static void abc(List<String> lists) {
-//        for (String lasts : lists) {
-//            Pattern patterns = Pattern.compile("[bd]");
-//            Matcher matchers = patterns.matcher(lasts);
-//            List<String> iss = new ArrayList<>();
-//            while (matchers.find()) {
-//                iss.add(matchers.group());
-//            }
-//            System.out.println(iss);
-//        }
+        System.out.println(words);
 
     }
 }
--- src/test/java/ua/lviv/iot/regex/TextCheckerTest.java
+++ src/test/java/ua/lviv/iot/regex/TextCheckerTest.java
@@ -1,18 +1,16 @@
 package ua.lviv.iot.regex;
 
-import org.junit.jupiter.api.Test;
-import ua.lviv.iot.regex.TextChecker;
-
 import java.util.Scanner;
 
 public class TextCheckerTest {
-    @Test
-    public void inputText() {
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("sada");
-//        System.out.print("Input a text: ");
-//        String text = in.nextLine();
-        String text = "aaaaedd dbklaeoctr";
-        TextChecker.checkPatterns(text);
+
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a text please: ");
+        String text = in.nextLine();
+        in.close();
+        String replaceText = text.replaceAll("[^A-Za-zА-Я\\s]", "");
+        TextChecker.checkPatterns(replaceText);
     }
+
 }
